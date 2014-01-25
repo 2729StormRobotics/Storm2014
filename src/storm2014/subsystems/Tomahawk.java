448b0939f7bd0143d05d13c6d88031de93ccc9ee
Garrett Beatty <gman9732@gmail.com>
2014-01-25 13:53:02 -0500

    added live window to tomahawk

--- src/storm2014/subsystems/Tomahawk.java
+++ src/storm2014/subsystems/Tomahawk.java
@@ -3,6 +3,7 @@ package storm2014.subsystems;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.Victor;
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 import storm2014.RobotMap;
 
 
@@ -11,6 +12,11 @@ public class Tomahawk extends Subsystem {
     
     DigitalInput _ir = new DigitalInput(RobotMap.PORT_SENSOR_TOMAHAWK_IR);
     Victor _motor = new Victor(RobotMap.PORT_MOTOR_TOMAHAWK);
+    
+   public Tomahawk(){
+       LiveWindow.addActuator("Tomahawk","Motor", _motor);
+       LiveWindow.addSensor("Tomahawk", "Sensor", _ir);
+   }
     
     protected void initDefaultCommand() {
         
@@ -22,4 +28,6 @@ public class Tomahawk extends Subsystem {
     public void setMotorRaw(double output){
         _motor.pidWrite(output);
     }
+    
+    
 }
