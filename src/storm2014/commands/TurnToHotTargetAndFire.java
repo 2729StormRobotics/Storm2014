d493a54581ce95af214b6f2040340cab43c7ce5f
ErikBobo <erikbobo@comcast.net>
2014-01-30 15:46:01 -0500

    TurnToHotTargetAndFire

--- src/storm2014/commands/TurnToHotTargetAndFire.java
+++ src/storm2014/commands/TurnToHotTargetAndFire.java
@@ -1,42 +1,31 @@
 package storm2014.commands;
 import edu.wpi.first.wpilibj.command.CommandGroup;
+import storm2014.subsystems.VisionSystem;
 /**
  *
  * @author Erik
  */
 public class TurnToHotTargetAndFire extends CommandGroup{
     private static final double ANGLE       = 75,
-                                TURN_SPEED  = 0.7;
+                                TURN_SPEED  = 0.7,
+                                DRIVE_SPEED = 0.6;   
+    public TurnToHotTargetAndFire(double sideLength) {
+       addSequential(new Conditional(new TurnLeftToHotTarget(ANGLE), new Engage()) {
+           protected boolean condition() {
+               return VisionSystem.foundHotTarget();
+           }
+       });
     
-    public TurnToHotTargetAndFire() {
-//       addSequential(new Conditional(new TurnLeftToHotTarget(ANGLE), new Engage()));
-//       addSequential(new Conditional(new TurnRightToHotTarget(ANGLE), new Engage()));
-       addSequential(new Engage());
-       addSequential(new Disengage());
+       addSequential(new Conditional(new TurnRightToHotTarget(ANGLE), new Engage()) {
+           protected boolean condition() {
+               return VisionSystem.foundHotTarget();
+           }
+       });
+      //addSequential(new Engage());
+      //addSequential(new Disengage());
+       
+       addSequential(new DriveForward(DRIVE_SPEED,sideLength));
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
-    }
-
-    // Called just before this Command runs the first time
-    protected void initialize() {
-    }
-
-    // Called repeatedly when this Command is scheduled to run
-    protected void execute() {
-    }
-
-    // Make this return true when this Command no longer needs to run execute()
-    protected boolean isFinished() {
-        return false;
-    }
-
-    // Called once after isFinished returns true
-    protected void end() {
-    }
-
-    // Called when another command which requires one or more of the same
-    // subsystems is scheduled to run
-    protected void interrupted() {
-    	end();
     }
   }
