8f6c7fa27f5e4a3b1ef81e6f36ad8b99d4e22ba1
ErikBobo <erikbobo@comcast.net>
2014-01-31 16:03:03 -0500

    TurnToHotTargetAndFire

--- src/storm2014/commands/TurnLeftToHotTarget.java
+++ src/storm2014/commands/TurnLeftToHotTarget.java
@@ -34,7 +34,7 @@ public class TurnLeftToHotTarget extends Command {
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return !VisionSystem.foundHotTarget(); 
+        return VisionSystem.foundHotTarget(); 
     }
 
     // Called once after isFinished returns true
