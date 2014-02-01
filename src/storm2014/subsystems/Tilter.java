6e45e598595ceb359ca92db9f7284cfb3d94b793
Evan Allan <evanallan1026@gmail.com>
2014-02-01 09:20:27 -0500

    Fixing more of Bobo's stuff. There's more to fix, but I don't know it and I'm busy

--- src/storm2014/subsystems/Tilter.java
+++ src/storm2014/subsystems/Tilter.java
@@ -15,7 +15,7 @@ public class Tilter extends Subsystem{
     
     private StringPot _stringpot = new StringPot(RobotMap.PORT_SENSOR_STRINGPOT);
     private Talon _talon = new Talon(RobotMap.PORT_MOTOR_TILTER);
-    private ADXL345_I2C _acc = new ADXL345_I2C();
+    private ADXL345_I2C _acc = new ADXL345_I2C(RobotMap.MODULE_SENSOR_ACCELEROMETER, ADXL345_I2C.DataFormat_Range.k2G);
     
     protected void initDefaultCommand() {
         
@@ -30,6 +30,6 @@ public class Tilter extends Subsystem{
     }
     
     public double getAccValue(){
-        return _acc.pidGet();
+        return _acc.pidGet(); //TODO get bobo to fix this
     }
 }
