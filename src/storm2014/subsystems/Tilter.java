afb87c3d6da03c0d361621a3b365c137df8b157a
Garrett Beatty <gman9732@gmail.com>
2014-02-06 17:38:24 -0500

    Deleted accelerometer in tilter

--- src/storm2014/subsystems/Tilter.java
+++ src/storm2014/subsystems/Tilter.java
@@ -15,7 +15,6 @@ public class Tilter extends Subsystem{
     
     private StringPot _stringpot = new StringPot(RobotMap.PORT_SENSOR_STRINGPOT);
     private Talon _talon = new Talon(RobotMap.PORT_MOTOR_TILTER);
-    private ADXL345_I2C _acc = new ADXL345_I2C(RobotMap.MODULE_SENSOR_ACCELEROMETER, ADXL345_I2C.DataFormat_Range.k2G);
     
     protected void initDefaultCommand() {
         
@@ -28,19 +27,7 @@ public class Tilter extends Subsystem{
     public double getVolts(){
         return _stringpot.get();
     }
-    
-    public double getAccValueX(){
-        return _acc.getAcceleration(ADXL345_I2C.Axes.kX); //TODO get bobo to fix this
-    } 
-    
-     public double getAccValueY(){
-        return _acc.getAcceleration(ADXL345_I2C.Axes.kY); //TODO get bobo to fix this
-    } 
-    
-     public double getAccValueZ(){
-        return _acc.getAcceleration(ADXL345_I2C.Axes.kZ); //TODO get bobo to fix this
-    } 
-     
+
     public double getAvgVoltage(){
         return _stringpot.get();
     }
