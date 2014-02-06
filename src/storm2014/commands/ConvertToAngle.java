e3c83ebea8ef62b07c0fecfade3e6e54d98e66b1
Garrett Beatty <gman9732@gmail.com>
2014-02-06 16:37:12 -0500

    Added convert to angle class skeleton.

---
+++ src/storm2014/commands/ConvertToAngle.java
@@ -0,0 +1,19 @@
+package storm2014.commands;
+
+/**
+ *
+ * @author garrett
+ */
+public class ConvertToAngle {
+ 
+    private double _angle;
+    
+    public ConvertToAngle(double angle){
+        _angle = angle;
+    }
+    
+    public double getAngle(){
+        return _angle;
+    }
+    
+}
