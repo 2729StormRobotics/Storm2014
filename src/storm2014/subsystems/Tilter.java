3c7a463168d3a678a6b0b20068b792fca6f7589e
Matthew Rassmann <matt.rassmann@me.com>
2014-02-06 17:54:57 -0500

    Fixes Tilter

--- src/storm2014/subsystems/Tilter.java
+++ src/storm2014/subsystems/Tilter.java
@@ -4,6 +4,8 @@ import edu.wpi.first.wpilibj.ADXL345_I2C;
 import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import storm2014.RobotMap;
+import storm2014.commands.ConvertToAngle;
+import storm2014.commands.Tilt;
 import storm2014.utilities.StringPot;
 
 /**
@@ -15,21 +17,18 @@ public class Tilter extends Subsystem{
     
     private StringPot _stringpot = new StringPot(RobotMap.PORT_SENSOR_STRINGPOT);
     private Talon _talon = new Talon(RobotMap.PORT_MOTOR_TILTER);
+    private ConvertToAngle _convert = new ConvertToAngle();
     
     protected void initDefaultCommand() {
-        
+        setDefaultCommand(new Tilt(0.0));
     }
     
     public void setRawVal(double speed){
         _talon.set(speed);
     }
     
-    public double getVolts(){
-        return _stringpot.get();
-    }
-
-    public double getAvgVoltage(){
-        return _stringpot.get();
+    public double getAngle(){
+        return  _convert.getAngle(_stringpot.get());
     }
     
 }
