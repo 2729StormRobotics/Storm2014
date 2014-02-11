01e73d0e0c2659756689e6fd50d3e8792d43583b
Garrett Beatty <gman9732@gmail.com>
2014-02-10 21:00:53 -0500

    Fixed joes sh*t.

--- src/storm2014/commands/WaitForFollowThrough.java
+++ src/storm2014/commands/WaitForFollowThrough.java
@@ -5,24 +5,40 @@ import storm2014.Robot;
 
 public class WaitForFollowThrough extends Command {
     private double _prevDiff  = 0,
-                   _prevAngle = 0;
-
+            _prevAngle = 0;
+    
     protected void initialize() {
         _prevDiff = _prevAngle = 0;
     }
     
     protected void execute() {}
-
+    
     protected boolean isFinished() {
         double angle = Robot.catapult.getPivotAngle();
-        double diff = angle-_prevAngle;
-        double product = diff*_prevDiff;
-        if(diff != 0) {
+        double diff = angle - _prevAngle;
+        _prevAngle = angle;
+        if(diff > 180){
+            diff -= sgn(diff) * 360;
+                    }
+        double product = diff * _prevDiff;
+        if(diff != 0){
             _prevDiff = diff;
         }
         return product < 0;
     }
-
+    
     protected void end() {}
     protected void interrupted() {}
+    
+    private static double sgn(double x){
+        if(x > 0){
+            return 1;
+        } else if(x==0){
+            return 0;
+        } else{
+            return -1;
+        }
+    }
 }
+
+
