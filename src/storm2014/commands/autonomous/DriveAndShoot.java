f25c5c51346ab39e10533371128d3ce807595b07
Tim Gordon(creativeNameHere) <tgordon555@comcast.net>
2014-02-22 13:33:29 -0500

    Made autonomous even better

--- src/storm2014/commands/autonomous/DriveAndShoot.java
+++ src/storm2014/commands/autonomous/DriveAndShoot.java
@@ -6,10 +6,13 @@
 
 package storm2014.commands.autonomous;
 
+import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.CommandGroup;
+import edu.wpi.first.wpilibj.command.WaitCommand;
 import storm2014.commands.DriveForward;
 import storm2014.commands.LaunchWhenReady;
 import storm2014.commands.control.Conditional;
+import storm2014.commands.control.DoNothing;
 import storm2014.subsystems.VisionSystem;
 
 /**
@@ -18,14 +21,27 @@ import storm2014.subsystems.VisionSystem;
  */
 public class DriveAndShoot extends CommandGroup{
     
+    private boolean foundHotTarget;
+    
     public DriveAndShoot(){
+        addSequential(new Command() {
+            protected void initialize() {
+                foundHotTarget = VisionSystem.foundHotTarget();
+            }
+            protected void execute() {}
+            protected boolean isFinished() {
+                return true;
+            }
+            protected void end() {}
+            protected void interrupted() {}
+        });
         addSequential(new DriveForward(0.75, 4700));
-        addSequential(new Conditional(new LaunchWhenReady(), new WaitAndShoot(5)) {
-            
+        addSequential(new Conditional(null, new WaitCommand(5)) { //may lower wait time on the waitcommand
             protected boolean condition() {
-                return VisionSystem.foundHotTarget();
+                return foundHotTarget;
             }
         });
+        addSequential(new LaunchWhenReady());
     }
     
 }
