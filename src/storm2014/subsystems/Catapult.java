f255867a1dc88c78155b6a54e2d4aa87cc004c49
Joe Doyle (Ginto8) <ginto8@gmail.com>
2014-02-28 17:40:44 -0500

    Rename 'Reset' state to 'Ready'

--- src/storm2014/subsystems/Catapult.java
+++ src/storm2014/subsystems/Catapult.java
@@ -200,7 +200,7 @@ public class Catapult extends Subsystem {
     
     public String getIndexName(){
         if(presetIndex == 0){
-            return "Reset";
+            return "Ready";
         }else{
             if(presetIndex == 1){
                 return "Short distance";
