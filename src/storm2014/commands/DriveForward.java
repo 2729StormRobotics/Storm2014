4f599ae11a59f557ad6f5cb001d7902d4559db0d
Joe Doyle (Ginto8) <ginto8@gmail.com>
2014-03-01 07:53:44 -0500

    Add proportional feedback control to DriveForward

--- src/storm2014/commands/DriveForward.java
+++ src/storm2014/commands/DriveForward.java
@@ -1,11 +1,16 @@
 package storm2014.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import storm2014.Robot;
 /**
  *Drives the robot forward by a certain distance and speed
  */
 public class DriveForward extends Command {
+    private static final double P_GAIN = 0;
+    static {
+        SmartDashboard.putNumber("Encoder feedback gain", P_GAIN);
+    }
     final double _speed;
     final double _distance;
 
@@ -20,7 +25,27 @@ public class DriveForward extends Command {
     }
 
     protected void execute() {
-        Robot.driveTrain.tankDrive(_speed*0.95, _speed);
+        double gain = SmartDashboard.getNumber("Encoder feedback gain",P_GAIN);
+        double err = Robot.driveTrain.getRightDistance()-Robot.driveTrain.getLeftDistance();
+        double diff = err*gain;
+        double left  = _speed + diff/2,
+               right = _speed - diff/2;
+        if(right < -1) {
+            left -= (right+1);
+            right = -1;
+        } else if(right > 1) {
+            left -= (right-1);
+            right = 1;
+        }
+        if(left < -1) {
+            right -= (left+1);
+            left = -1;
+        } else if(left > 1) {
+            right -= (left-1);
+            left = 1;
+        }
+        Robot.driveTrain.tankDrive(Math.max(-1, Math.min(1, left )),
+                                   Math.max(-1, Math.min(1, right)));
     }
 
     protected boolean isFinished() {
